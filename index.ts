73a4b357056b56c6b6d0844808f2a242e7d44b46
Maximilian Beck <contact@maximilianbeck.de>
2020-10-14 15:12:00 +0200

    feat: Export createAwaitAction in index file

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 export { useAwaitAction } from './src/useAwaitAction';
 export { withAwaitAction } from './src/withAwaitAction';
 export { createAwaitMiddleware } from './src/createAwaitMiddleware';
+export { createAwaitAction, StoreAwait } from './src/createAwaitAction';
 export { StoreAwaitEventEmitter } from './src/StoreAwaitEventEmitter';
 export { StoreAwaitProvider, AwaitEventEmitterContext } from './src/context/awaitEventEmitterContext';
