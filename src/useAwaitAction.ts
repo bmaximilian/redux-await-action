435ec6931649d94fb8b3d901b6d9f19f7d992a53
FaBeyyy <fabiofesel5@googlemail.com>
2022-08-11 14:28:56 +0200

    Remove type assertion

--- src/useAwaitAction.ts
+++ src/useAwaitAction.ts
@@ -10,7 +10,7 @@ import { ActionTypes, createAwaitAction } from './createAwaitAction';
  * @returns - A function to wait for specific actions
  */
 export function useAwaitAction<S extends Action = Action>(): (s: ActionTypes, e?: ActionTypes) => Promise<S> {
-    const store = useStore<any, S>();
+    const store = useStore();
     const eventEmitter = useContext(AwaitEventEmitterContext);
 
     if (!eventEmitter) throw new Error('You need to access the await inside the <StoreAwaitProvider> component');
